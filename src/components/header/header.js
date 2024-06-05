389db7738f239b23381e32b5dd8b713702bc7e4c
Quick873 <116125610+Quick873@users.noreply.github.com>
2024-06-04 21:07:12 -0500

    React give me a headache

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from '../../../public/logo.jpg'
+import logo from '../assets/logo.jpg';
 import './header.css';
 
 
@@ -10,10 +10,11 @@ export default function Header () {
     return (
         <div className="header">
             <img src={logo} className="header-logo" alt="logo"/>
-            <NavLink to={'/about'}>About Us</NavLink>
+            {/*<NavLink to={'/about'}>About Us</NavLink>
             <NavLink to={'/menu'}>Menu</NavLink>
             <NavLink to={'/order'}>Order Online</NavLink>
-            <NavLink to={'contact'}>Contact Us</NavLink>
+    <NavLink to={'contact'}>Contact Us</NavLink>*/}
+            <h1>React gives me a headache!</h1>
         </div>
     )
 }
