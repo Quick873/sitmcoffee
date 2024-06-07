bf6bc27962b7654efd682ceb72a99761b61c5a75
Quick873 <116125610+Quick873@users.noreply.github.com>
2024-06-06 20:21:21 -0500

    AAAAAAAAAHHHHHHHHH

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-
+import Root from './components/root/Root';
 import './App.css';
 import React from 'react';
-import { Route, RouterProvider } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import About from './components/about/About';
+import Menu from './components/menu/Menu';
+import Order from './components/order/Order';
+import Contact from './components/contact/Contact';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={ Root }>
+    <Route path='about' element={ About }/>
+    <Route path='menu' element={ Menu } />
+    <Route path='order' element={ Order } />
+    <Route path='contact' element={ Contact } />
+  </Route>
+));
 
 function App() {
   return (
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from '../assets/logo.jpg';
-import './header.css';
+import './Header.css';
 
 
 export default function Header () {
@@ -10,10 +10,10 @@ export default function Header () {
     return (
         <div className="header">
             <img src={logo} className="header-logo" alt="logo"/>
-            {/*<NavLink to={'/about'}>About Us</NavLink>
+            <NavLink to={'/about'}>About Us</NavLink>
             <NavLink to={'/menu'}>Menu</NavLink>
             <NavLink to={'/order'}>Order Online</NavLink>
-    <NavLink to={'contact'}>Contact Us</NavLink>*/}
+            <NavLink to={'contact'}>Contact Us</NavLink>
             <h1>React gives me a headache!</h1>
         </div>
     )
